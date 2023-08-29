02c59c71accbdef9719c3511f13a567cd5ecafd2
jopimi <josepitarch98@gmail.com>
2023-08-29 21:50:33 +0200

    add supabase types

---
+++ src/types/supabase.ts
@@ -0,0 +1,91 @@
+export interface Database {
+  public: {
+    Tables: {
+      allergens: {
+        Row: {
+          id: number
+          name: string
+        }
+        Insert: {
+          id?: number
+          name: string
+        }
+        Update: {
+          id?: number
+          name?: string
+        }
+        Relationships: []
+      }
+      brands: {
+        Row: {
+          id: number
+          name: string
+        }
+        Insert: {
+          id?: number
+          name: string
+        }
+        Update: {
+          id?: number
+          name?: string
+        }
+        Relationships: []
+      }
+      foods: {
+        Row: {
+          brand: number | null
+          carbs: number
+          fats: number
+          id: number
+          kilocalories: number
+          name: string
+          proteins: number
+          quantity: number
+          unit: string
+        }
+        Insert: {
+          brand?: number | null
+          carbs?: number
+          fats?: number
+          id?: number
+          kilocalories: number
+          name: string
+          proteins?: number
+          quantity?: number
+          unit: string
+        }
+        Update: {
+          brand?: number | null
+          carbs?: number
+          fats?: number
+          id?: number
+          kilocalories?: number
+          name?: string
+          proteins?: number
+          quantity?: number
+          unit?: string
+        }
+        Relationships: [
+          {
+            foreignKeyName: "foods_brand_fkey"
+            columns: ["brand"]
+            referencedRelation: "brands"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
+    }
+    Views: {
+      [_ in never]: never
+    }
+    Functions: {
+      [_ in never]: never
+    }
+    Enums: {
+      [_ in never]: never
+    }
+    CompositeTypes: {
+      [_ in never]: never
+    }
+  }
+}
