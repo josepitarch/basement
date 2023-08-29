bc2161c0bed3a29accfa17e630dac042014fec9e
jopimi <josepitarch98@gmail.com>
2023-08-29 21:53:57 +0200

    add common layout

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,12 +1,9 @@
-type Props = {
-  children: React.ReactNode
-}
-  
-
-export default function Layout({ children }: Props) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className='max-w-[1200px] m-auto lg:-translate-x-28'>
-      <main className='flex justify-center p-6 animate-fade-up text-white'>{children}</main>
+      <main className='flex justify-center p-6 animate-fade-up text-white'>
+        {children}
+      </main>
     </div>
   )
 }
