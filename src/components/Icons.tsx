52e3325252b064655047f9c3176c62b28eb41872
jopimi <josepitarch98@gmail.com>
2023-08-29 21:56:53 +0200

    add icons component

---
+++ src/components/Icons.tsx
@@ -0,0 +1,11 @@
+export const AddFoodIcon = () => (
+  <svg viewBox='0 0 24 24' fill='none'>
+    <path
+      d='M4 12h16m-8-8v16'
+      stroke='#000'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+  </svg>
+)
