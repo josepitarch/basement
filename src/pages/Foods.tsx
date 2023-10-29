40938a729d6b61c61c95ed99ffaaa9e927292fc1
jopimi <josepitarch98@gmail.com>
2023-10-29 19:43:49 +0100

    show macros on page

--- src/pages/Foods.tsx
+++ src/pages/Foods.tsx
@@ -1,32 +1,50 @@
-import { useContext, useEffect } from 'react'
+import { useEffect } from 'react'
 import { useLocation } from 'wouter'
-import { AddFoodIcon } from '../components/Icons'
+import { Icons } from '../components/Icons'
 import MealItem from '../components/MealItem'
-import { MacrosContext } from '../context/MacrosContext'
-import useDiet from '../hooks/useDiet'
 import Layout from '../layouts/Layout'
+import { useDietStore } from '../store/diet'
+import { useCustomizeMacrosStore } from '../store/macros'
 
-export default function Foods() {
+export default function Foods () {
   const [, setLocation] = useLocation()
-  const { macros } = useContext(MacrosContext)
-  const { diet, addMeal } = useDiet()
+  const { macros } = useCustomizeMacrosStore()
+  const { diet, addMeal } = useDietStore()
 
   useEffect(() => {
-    if (!macros) setLocation('/')
+    if (!macros) {
+      setLocation('/customize-fats-and-proteins')
+    }
   }, [macros])
-
 
   const handleAddMeal = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
     const formData = new FormData(event.currentTarget)
-    const meal = formData.get('input-meal') as string
-    if (!meal) return
+    const nameMeal = formData.get('input-meal')?.toString()
+    if (!nameMeal) return
 
-    addMeal({ name: meal })
+    addMeal(nameMeal)
   }
 
+  const macros1 = diet.meals.reduce((acc, meal) => {
+    meal.options.forEach((option) => {
+      option.foods.forEach((food) => {
+        acc.kilocalories += (food.quantity * food.food.kilocalories) / food.food.quantity
+        acc.fats += (food.quantity * food.food.fats) / food.food.quantity
+        acc.carbs += (food.quantity * food.food.carbs) / food.food.quantity
+        acc.proteins += (food.quantity * food.food.proteins) / food.food.quantity
+      })
+    })
+    return acc
+  }, {
+    kilocalories: 0,
+    fats: 0,
+    carbs: 0,
+    proteins: 0
+  })
+
   return (
-    <Layout>
+    <Layout title='Tu dieta'>
       <div className='flex flex-col w-full gap-6'>
         <h1>Foods</h1>
         <form className='flex flex-row max-w-2xl' onSubmit={handleAddMeal}>
@@ -49,13 +67,34 @@ export default function Foods() {
               className='absolute w-full h-full opacity-0 cursor-pointer z-10'
               aria-label='Añadir comida'
             />
-            <AddFoodIcon />
+            <Icons.add />
           </label>
         </form>
 
+        <div className={`flex flex-row gap-2 ${diet.meals.length > 0 ? 'visible' : 'invisible'}`}>
+          <article className='flex flex-col'>
+            <span>Kilocalorías </span>
+            <span>
+              {macros1.kilocalories.toFixed(0)}
+            </span>
+          </article>
+          <article className='flex flex-col'>
+            <span>Grasas </span>
+            <span>{macros1.fats.toFixed(0)}</span>
+          </article>
+          <article className='flex flex-col'>
+            <span>Carbohidratos </span>
+            <span>{macros1.carbs.toFixed(0)}</span>
+          </article>
+          <article className='flex flex-col'>
+            <span>Proteínas </span>
+            <span>{macros1.proteins.toFixed(0)}</span>
+          </article>
+        </div>
+
         <div className='text-white relative w-full space-y-3 lg:space-y-5 lg:w-1/2 lg:pr-4'>
           {diet.meals.map((meal) => (
-            <MealItem meal={meal} />
+            <MealItem key={meal.id} meal={meal} />
           ))}
         </div>
       </div>
