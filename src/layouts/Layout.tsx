7593e3c62165cd7b3bfc144ca7859db5e42210aa
jopimi <josepitarch98@gmail.com>
2023-10-29 19:40:42 +0100

    add title parameter

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,7 +1,13 @@
-export default function Layout({ children }: { children: React.ReactNode }) {
+import { useEffect } from 'react'
+
+export default function Layout ({ title = 'The Basement', children }: { title?: string, children: React.ReactNode }) {
+  useEffect(() => {
+    document.title = title
+  }, [title])
+
   return (
     <div className='max-w-[1200px] m-auto lg:-translate-x-28'>
-      <main className='flex justify-center p-6 animate-fade-up text-white'>
+      <main className='flex justify-center p-6 animate-fade-up text-black'>
         {children}
       </main>
     </div>
